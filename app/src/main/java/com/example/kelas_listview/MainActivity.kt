cb4caa8b39c7292cb714ffdeb0ad1cc3032d3620
Mike <131933991+privmike@users.noreply.github.com>
2025-10-28 22:00:52 +0700

    selesai untuk file kelas listview

--- app/src/main/java/com/example/kelas_listview/MainActivity.kt
+++ app/src/main/java/com/example/kelas_listview/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.kelas_listview
 
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var data = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,124 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        data.addAll(listOf("1","2","3","4","5","6"))
+
+        val lvadapter : ArrayAdapter<String> = ArrayAdapter(this,android.R.layout.simple_list_item_1,data)
+
+
+        val lv1 : ListView = findViewById<ListView>(R.id.lv1)
+        lv1.adapter = lvadapter
+
+        val btnTambah = findViewById<Button>(R.id.btnTambah)
+        btnTambah.setOnClickListener {
+            var dtAkhir = Integer.parseInt(data.get(data.size-1)) +1
+            data.add("$dtAkhir")
+            lvadapter.notifyDataSetChanged()
+        }
+        lv1.setOnItemClickListener { parent, view, position,id ->
+            Toast.makeText(this, data[position], Toast.LENGTH_SHORT).show()
+        }
+
+        val gestureDetector = GestureDetector (
+            this,
+            object : GestureDetector.SimpleOnGestureListener(){
+                override fun onDoubleTap(e: MotionEvent): Boolean {
+                    val position = lv1.pointToPosition(e.x.toInt(), e.y.toInt())
+                    if (position!= ListView.INVALID_POSITION){
+                        val selecteditem = data[position]
+                        showActionDialog(position, selecteditem, data, lvadapter)
+                    }
+                    return true
+                }
+            }
+        )
+        lv1.setOnTouchListener { _, event ->
+            gestureDetector.onTouchEvent(event)
+        }
+        lvadapter.notifyDataSetChanged()
+
+
+    }
+
+    private fun showActionDialog(
+        position: Int,
+        selectedItem  : String,
+        data : MutableList<String>,
+        adapter : ArrayAdapter<String>
+    ){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("ITEM $selectedItem")
+        builder.setMessage("Pilih tindakan yang ingin dilakukan")
+
+        builder.setPositiveButton("Update") { _,_ ->
+            showUpdateDialog(position,selectedItem,data,adapter)
+        }
+        builder.setNegativeButton("Hapus") { _, _ ->
+            data.removeAt(position)
+            adapter.notifyDataSetChanged()
+            Toast.makeText(
+                this,
+                "Hapus teks $selectedItem",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        builder.setNeutralButton("Batal"){dialog ,_ ->
+            dialog.dismiss()
+        }
+        builder.create().show()
+    }
+
+
+    private fun showUpdateDialog(
+        position: Int,
+        oldvalue  : String,
+        data : MutableList<String>,
+        adapter : ArrayAdapter<String>
+    ){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Update Data")
+
+        val layout = LinearLayout(this)
+        layout.orientation = LinearLayout.VERTICAL
+        layout.setPadding(50,40,50,10)
+
+        val textviewOld = TextView(this)
+        textviewOld.text = "Data Lama : $oldvalue"
+        textviewOld.textSize = 16f
+
+        val editext = EditText(this)
+        editext.hint = "Masukan data baru"
+        editext.setText(oldvalue)
+
+        layout.addView(textviewOld)
+        layout.addView(editext)
+
+        builder.setView(layout)
+
+        builder.setPositiveButton("Simpan"){ dialog, _ ->
+            val newvalue = editext.text.toString().trim()
+            if (newvalue.isNotEmpty()){
+                data[position] = newvalue
+                adapter.notifyDataSetChanged()
+                Toast.makeText(
+                    this,
+                    "Data Diupdate jadi $newvalue",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }else{
+                Toast.makeText(
+                    this,
+                    "Data baru tidak boleh kosong",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        builder.setNegativeButton("Batal"){ dialog, _ ->
+            dialog.dismiss()
+        }
+
+        builder.create().show()
     }
 }
